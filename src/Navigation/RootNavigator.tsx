982488a2cb97fa1f7d7026bf1030b616f9f3dff5
wadekar9 <npwadekar7@gmail.com>
2023-04-18 23:31:31 +0530

    Little Navigation Setup

--- src/Navigation/RootNavigator.tsx
+++ src/Navigation/RootNavigator.tsx
@@ -1,11 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
+
+import HomeScreen from '../Screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
     return (
-        <View>
-            <Text>RootNavigator</Text>
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name='Home'
+                    component={HomeScreen}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
     )
 }
 
