e514c53869032497ffd5ad441bea6d11ca573720
Sridix Tech <team.sridix@gmail.com>
2023-04-18 23:20:06 +0530

    intial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RootNavigator from './src/Navigation/RootNavigator';
+
+const App = () => {
+  return (
+    <>
+      <RootNavigator />
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/CommonContainer.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, View, Text } from 'react-native'
+import React from 'react';
+
+const CommonContainer = ({children}) => {
+  return (
+    <SafeAreaView style={{flex : 1,backgroundColor : '#fff'}}>
+        <View style={{flex : 1}}>
+            {children}
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default CommonContainer;
