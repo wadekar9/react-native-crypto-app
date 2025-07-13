64573e972a02b7b3118d6ed5bfce6a6454e407fa
wadekar9 <npwadekar7@gmail.com>
2025-07-13 23:14:12 +0530

    feat: add custom hooks for app theme, auto image handling, debounce functionality

---
+++ src/hooks/common/index.ts
@@ -0,0 +1,4 @@
+export { useAutoImage } from './auto-image.hook';
+export { useAppTheme } from './app-theme.hook';
+export { useDebounce } from './debounce.hook';
+export { useSafeAreaInsetsStyle } from './safearea-styles.hook';
