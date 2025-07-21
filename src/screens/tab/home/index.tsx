85d1a7a1d7e5604066ee12176a1c7c78358c2a07
wadekar9 <npwadekar7@gmail.com>
2025-07-21 22:15:30 +0530

    feat : implemented skeleton for home and market screens

--- src/screens/tab/home/index.tsx
+++ src/screens/tab/home/index.tsx
@@ -7,40 +7,44 @@ import { EBottomTabScreens, EStackScreens } from '$constants/screens.constants';
 import { BaseLayout, TextButton } from '$components/common';
 import { BaseCoinListItem, BaseNFTListItem } from '$components/layouts';
 import { useHomePageDetails } from '$hooks/modules';
+import { TrendingHomeScreenSkeleton } from '$components/skeleton';
 
 const Home: React.FC<BottomTabsScreenProps<EBottomTabScreens.HOME>> = () => {
 
-  const { details, loading, fetchHomeDetails } = useHomePageDetails();
+  const { details, loading } = useHomePageDetails();
 
   return (
     <BaseLayout>
-      <ScrollView
-        contentContainerStyle={styles.contentContainer}
-        scrollEventThrottle={16}
-      >
-        <View style={styles.container}>
-          <View style={styles.header}>
-            <Text style={styles.labelStyle}>Trending Cryptocurrencies</Text>
-            <TextButton label='View All' onPress={() => navigationRef.current?.navigate(EStackScreens.TRENDING_COINS_NFTS, { coins: JSON.stringify(details.coins), nfts: JSON.stringify([]) })} />
+      {loading && <TrendingHomeScreenSkeleton />}
+      {!loading && (
+        <ScrollView
+          contentContainerStyle={styles.contentContainer}
+          scrollEventThrottle={16}
+        >
+          <View style={styles.container}>
+            <View style={styles.header}>
+              <Text style={styles.labelStyle}>Trending Cryptocurrencies</Text>
+              <TextButton label='View All' onPress={() => navigationRef.current?.navigate(EStackScreens.TRENDING_COINS_NFTS, { coins: JSON.stringify(details.coins), nfts: JSON.stringify([]) })} />
+            </View>
+            <View style={styles.content}>
+              {details.coins.slice(0, 5).map((coin, idx) => (
+                <BaseCoinListItem key={`${idx}`} element={coin} />
+              ))}
+            </View>
           </View>
-          <View style={styles.content}>
-            {details.coins.slice(0, 5).map((coin, idx) => (
-              <BaseCoinListItem key={`${idx}`} element={coin} />
-            ))}
+          <View style={styles.container}>
+            <View style={styles.header}>
+              <Text style={styles.labelStyle}>Trending NFTs</Text>
+              <TextButton label='View All' onPress={() => navigationRef.current?.navigate(EStackScreens.TRENDING_COINS_NFTS, { coins: JSON.stringify([]), nfts: JSON.stringify(details.nfts) })} />
+            </View>
+            <View style={styles.content}>
+              {details.nfts.slice(0, 5).map((coin, idx) => (
+                <BaseNFTListItem key={`${idx}`} element={coin} />
+              ))}
+            </View>
           </View>
-        </View>
-        <View style={styles.container}>
-          <View style={styles.header}>
-            <Text style={styles.labelStyle}>Trending NFTs</Text>
-            <TextButton label='View All' onPress={() => navigationRef.current?.navigate(EStackScreens.TRENDING_COINS_NFTS, { coins: JSON.stringify([]), nfts: JSON.stringify(details.nfts) })} />
-          </View>
-          <View style={styles.content}>
-            {details.nfts.slice(0, 5).map((coin, idx) => (
-              <BaseNFTListItem key={`${idx}`} element={coin} />
-            ))}
-          </View>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      )}
     </BaseLayout>
   )
 }
--- src/screens/tab/market/index.tsx
+++ src/screens/tab/market/index.tsx
@@ -7,9 +7,11 @@ import { IGlobalMarketData } from '$types/api-types';
 import apiService from '$utils/api';
 import { showErrorFlashMessage } from '$utils/helpers';
 import { MarketPageContent, MarketPageHeader, MarketPageTabNavigator } from '$components/layouts';
+import { MarketOverviewSkeleton } from '$components/skeleton';
 
 const Market: React.FC<BottomTabsScreenProps<EBottomTabScreens.MARKET>> = () => {
 
+  const [loading, setLoading] = useState<boolean>(true);
   const [marketDetails, setMarketDetails] = useState<IGlobalMarketData | null>(null);
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const scrollViewRef = useRef<ScrollView>(null);
@@ -17,25 +19,33 @@ const Market: React.FC<BottomTabsScreenProps<EBottomTabScreens.MARKET>> = () => 
   useEffect(() => {
     (async () => {
       try {
+        setLoading(true);
         const response = await apiService.fetchGlobalMarketDataApi();
         if (response) {
           setMarketDetails(response.data);
         }
       } catch (error) {
         showErrorFlashMessage(error);
+      } finally {
+        setLoading(false);
       }
     })();
   }, []);
 
   return (
     <BaseLayout>
-      <MarketPageHeader marketDetails={marketDetails} />
-      <MarketPageTabNavigator
-        currentIndex={currentIndex}
-        setCurrentIndex={setCurrentIndex}
-        scrollViewRef={scrollViewRef}
-      />
-      <MarketPageContent currentIndex={currentIndex} setCurrentIndex={setCurrentIndex} />
+      {loading && <MarketOverviewSkeleton />}
+      {!loading && (
+        <>
+          <MarketPageHeader marketDetails={marketDetails} />
+          <MarketPageTabNavigator
+            currentIndex={currentIndex}
+            setCurrentIndex={setCurrentIndex}
+            scrollViewRef={scrollViewRef}
+          />
+          <MarketPageContent currentIndex={currentIndex} setCurrentIndex={setCurrentIndex} />
+        </>
+      )}
     </BaseLayout>
   );
 };
