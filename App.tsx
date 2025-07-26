3225ac2904b2f83166ca454784ff56a3bdf18383
wadekar9 <npwadekar7@gmail.com>
2025-07-27 00:13:48 +0530

    feat: implement splash screen with RNBootSplash and update app configuration

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { navigationRef } from '$types/navigation';
 import RootNavigator from '$navigation/root-navigator';
 import AppThemeProvider from '$context/app-theme.context';
 import FlashMessage from 'react-native-flash-message';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import RNBootSplash from 'react-native-bootsplash';
 
 const App: React.FC = () => {
+
+  useEffect(() => {
+    RNBootSplash.hide({ fade: true });
+  }, [])
+
   return (
     <AppThemeProvider>
       <SafeAreaProvider style={{ flex: 1 }}>
--- android/app/src/main/java/com/cryptoapp/MainActivity.kt
+++ android/app/src/main/java/com/cryptoapp/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.cryptoapp;
-
-import android.os.Bundle;
+import android.os.Bundle
+import com.zoontek.rnbootsplash.RNBootSplash
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -9,7 +9,8 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 class MainActivity : ReactActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(null)
+    RNBootSplash.init(this, R.style.BootTheme) // ⬅️ initialize the splash screen
+    super.onCreate(savedInstanceState) // super.onCreate(null) with react-native-screens
   }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
